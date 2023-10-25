c020b203398161f569fdd3e0b283175aa5f7cecf
Sakhile123 <115566123+Sakhile123@users.noreply.github.com>
2023-10-25 20:16:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+const menuIcon = document.querySelector('.menu-icon');
+const menu = document.querySelector('.menu');
+
+menuIcon.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
